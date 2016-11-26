95ee7399b7bcf15b1d152535c2180110e8dda9e4
ralleg314 <ralleg314@gmail.com>
2016-11-26 13:45:17 +0100

    DS-26-11-16_1

---
+++ Lab4/Code/TotSeriesParser/src/edu/ub/informatica/disseny/model/Administrador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ub.informatica.disseny.model;
+
+/**
+ *
+ * @author ralleg
+ */
+public class Administrador {
+    private Afegir add;
+    public boolean ferVip(Usuari_registrat usuari){
+        return false;
+    }
+    
+    public boolean afegir(String tipus){
+        return false;
+    }
+}
